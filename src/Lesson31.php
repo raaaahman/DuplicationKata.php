40a860828cbd27dcb5d555dc807950924dcb2c60
raaaahman <sylvain@devindetails.com>
2020-04-04 11:34:22 +0200

    Replaced conditional by polymorphism

--- src/Lesson31.php
+++ src/Lesson31.php
@@ -8,17 +8,11 @@ class Lesson31 extends Song
         $style2 = new SongPatternStyle2();
         $style3 = new SongPatternStyle3();
 
-        switch ($style) {
-            case 1 :
-                $this->singPattern( $style1->repeatFor($names) );
-                break;
-            case 2 :
-                $this->singPattern( $style2->repeatFor($names) );
-                break;
-            case 3 :
-                $this->singPattern( $style3->repeatFor($names)) ;
-                break;
-        }
+        $styles = [ $style1, $style2, $style3 ];
+
+        $selectedStyle = $styles[$style-1];
+
+        $this->singPattern($selectedStyle->repeatFor($names));
     }
 
 
