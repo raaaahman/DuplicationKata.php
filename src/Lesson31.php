e3a4ce082ea4a9aff00baa1aefd7c84101cb7feb
raaaahman <sylvain@devindetails.com>
2020-04-04 11:44:56 +0200

    Replaced subclasses for style 1 and 3 with parameterized object

--- src/Lesson31.php
+++ src/Lesson31.php
@@ -4,15 +4,27 @@ class Lesson31 extends Song
 {
     public function singSong($style, $names)
     {
-        $style1 = new SongPatternStyle1();
-        $style2 = new SongPatternStyle2();
-        $style3 = new SongPatternStyle3();
-
-        $styles = [ $style1, $style2, $style3 ];
-
-        $selectedStyle = $styles[$style-1];
+        $selectedStyle = $this->selectStyle($style);
 
         $this->singPattern($selectedStyle->repeatFor($names));
+    }
+
+    /**
+     * @param $style
+     * @return mixed
+     */
+    public function selectStyle($style)
+    {
+        $defaultLine = "Hello %s, it's nice to meet you.";
+        $style1 = new SongPatternWithStyle($defaultLine);
+        $style1->registerSpecialLine("Hip Hip Horray! For %s")->when(Word::startsWith("L"));
+        $style2 = new SongPatternStyle2();
+        $style3 = new SongPattern($defaultLine);
+
+        $styles = [$style1, $style2, $style3];
+
+        $selectedStyle = $styles[$style - 1];
+        return $selectedStyle;
     }
 
 
